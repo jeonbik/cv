79c6ae7f20d3dbce8ba45774bcf100cf2b8e8f35
Bikram Khanal <bikram.khanal@aiesec.net>
2024-02-08 14:58:40 -0600

    Updated Google Scholar Icon

--- src/components/icons/GoogleScholarIcon.tsx
+++ src/components/icons/GoogleScholarIcon.tsx
@@ -1,11 +1,10 @@
 export const GoogleScholarIcon = (props: React.SVGProps<SVGSVGElement>) => {
-    return (
-      <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
-        <path
-          fill="currentColor"
-          d="M12,2A10,10 0 1,0 22,12A10,10 0 0,0 12,2ZM17,9L12,12.5L7,9L7.5,14H11V19L13,18L15,19V14H18.5L17,9Z"
-        />
-      </svg>
-    );
-  };
-  
+  return (
+    <svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" {...props}>
+      {/* Outer box */}
+      <rect width="24" height="24" rx="3" fill="currentColor"/>
+      {/* Letter 'g' */}
+      <text x="50%" y="50%" dy=".35em" fill="white" fontSize="14" fontFamily="Arial, sans-serif" textAnchor="middle">g</text>
+    </svg>
+  );
+};
