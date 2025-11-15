d520f5cf3b811e4ebf13a705707cfc8469a560a4
jeonbik <36014522+jeonbik@users.noreply.github.com>
2025-11-15 13:04:07 -0800

    fix: optional chain project link in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -210,7 +210,7 @@ export default function Page() {
                   title={project.title}
                   description={project.description}
                   tags={project.techStack}
-                  link={"link" in project ? project.link.href : undefined}
+                  link={project.link?.href}
                 />
               );
             })}
